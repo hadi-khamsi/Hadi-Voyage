e9be76a3559d426fee5fe13c9ef5e8580944bd15
Hadi Khamsi <hadik8360@gmail.com>
2024-07-03 23:04:25 -0500

    fix routing

--- src/index.js
+++ src/index.js
@@ -5,10 +5,25 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <Router basename="/Hadi-Voyage">
+  <Router>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </Router>,
   document.getElementById("root")
 );
+
+/*import React from "react";
+import ReactDOM from "react-dom";
+import "./tailwind.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+
+ReactDOM.render(
+  <Router basename="/Hadi-Voyage">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>,
+  document.getElementById("root")
+);*/
